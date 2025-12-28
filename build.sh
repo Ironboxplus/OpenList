aa5484ca0851d959913b206d070f0d41160aba4a
cyk <dr_arc@163.com>
2025-12-28 23:16:46 +0800

    refactor(build): restrict builds to x64 architecture and simplify Docker workflow

--- build.sh
+++ build.sh
@@ -186,8 +186,8 @@ BuildDockerMultiplatform() {
   docker_lflags="--extldflags '-static -fpic' $ldflags"
   export CGO_ENABLED=1
 
-  OS_ARCHES=(linux-amd64 linux-arm64 linux-386 linux-riscv64 linux-ppc64le linux-loong64) ## Disable linux-s390x builds
-  CGO_ARGS=(x86_64-linux-musl-gcc aarch64-linux-musl-gcc i486-linux-musl-gcc riscv64-linux-musl-gcc powerpc64le-linux-musl-gcc loongarch64-linux-musl-gcc) ## Disable s390x-linux-musl-gcc builds
+  OS_ARCHES=(linux-amd64) ## Disable linux-s390x builds
+  CGO_ARGS=(x86_64-linux-musl-gcc) ## Disable s390x-linux-musl-gcc builds
   for i in "${!OS_ARCHES[@]}"; do
     os_arch=${OS_ARCHES[$i]}
     cgo_cc=${CGO_ARGS[$i]}
@@ -205,14 +205,14 @@ BuildDockerMultiplatform() {
   GO_ARM=(6 7)
   export GOOS=linux
   export GOARCH=arm
-  for i in "${!DOCKER_ARM_ARCHES[@]}"; do
-    docker_arch=${DOCKER_ARM_ARCHES[$i]}
-    cgo_cc=${CGO_ARGS[$i]}
-    export GOARM=${GO_ARM[$i]}
-    export CC=${cgo_cc}
-    echo "building for $docker_arch"
-    go build -o build/${docker_arch%%-*}/${docker_arch##*-}/"$appName" -ldflags="$docker_lflags" -tags=jsoniter .
-  done
+  # for i in "${!DOCKER_ARM_ARCHES[@]}"; do
+  #   docker_arch=${DOCKER_ARM_ARCHES[$i]}
+  #   cgo_cc=${CGO_ARGS[$i]}
+  #   export GOARM=${GO_ARM[$i]}
+  #   export CC=${cgo_cc}
+  #   echo "building for $docker_arch"
+  #   go build -o build/${docker_arch%%-*}/${docker_arch##*-}/"$appName" -ldflags="$docker_lflags" -tags=jsoniter .
+  # done
 }
 
 BuildRelease() {
